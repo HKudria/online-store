6632162904a69d39dcac06f9c24b145bdd129148
AnyaPolischuk <polisuk092@gmail.com>
2022-12-27 17:57:45 +0300

    feat: add description of product and bread crumbs

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,45 +1,81 @@
 import s from './ProductPage.module.css';
-import iPhone from '../../assets/image/iphone.png';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { getProductsState, parseProducts } from '../../redux/products/productsSlice';
+import { useLocation } from 'react-router';
+import { useEffect, useState } from 'react';
+import { IProduct } from '../../redux/products/ProductInterface';
+import { NavLink } from 'react-router-dom';
+
 
 export const ProductPage = () => {
+  const {products} = useAppSelector(getProductsState);
+  const dispatch = useAppDispatch();
+  const location = useLocation();
+  const defaultProduct: IProduct[] = [{
+    id: 0,
+    title: '',
+    description: '',
+    discountPercentage: 0,
+    price: 0,
+    rating: 0,
+    stock: 0,
+    brand: '',
+    category: '',
+    thumbnail: '',
+    images: [],
+  }]
+
+
+  const [product, setProduct] = useState(defaultProduct);
+  
+  useEffect(() => {
+    dispatch(parseProducts());
+  }, []);
+
+
+  useEffect(() => {
+    const routeList = location.pathname.split('/');
+    const path = routeList[routeList.length - 1];
+    const currentProduct = products.filter((item: IProduct) => item.id === +path);
+    setProduct(currentProduct);
+  })
+
   return (
     <div className={s.wrapper}>
       <div className={s.breadCrumbs}>
-        <p>Store</p>
+        <NavLink to='/'>Store</NavLink>
         <p>--</p>
-        <p>Smartfones</p>
+        <p>{product[0].category}</p>
         <p>--</p>
-        <p>Apple</p>
+        <p>{product[0].brand}</p>
         <p>--</p>
-        <p>Iphone</p>
+        <p>{product[0].title}</p>
       </div>
       <div className={s.content}>
-        <div className={s.productName}>IPhone 9</div>
+        <div className={s.productName}>{product[0].title}</div>
         <div className={s.photo}>
           <div className={s.miniPhoto}>
-            <img className={s.miniPhotoItem} src={iPhone}></img>
-            <img className={s.miniPhotoItem} src={iPhone}></img>
-            <img className={s.miniPhotoItem} src={iPhone}></img>
+            {product[0].images.map((item, index) => <img key={index} className={s.miniPhotoItem} src={item}></img>)}
           </div>
           <div className={s.bigPhoto}>
-            <img className={s.bigPhotoItem} src={iPhone}></img>
+            <img className={s.bigPhotoItem} src={product[0].images[0]}></img>
           </div>
         </div>
         <div className={s.description}>
-          <p>Description: <span className={s.desctiptionItem}>test1</span></p>
-          <p>Discount Percentage: <span className={s.desctiptionItem}>test2</span></p>
-          <p>Rating: <span className={s.desctiptionItem}>test3</span></p>
-          <p>Stock: <span className={s.desctiptionItem}>test4</span></p>
-          <p>Brand: <span className={s.desctiptionItem}>test5</span></p>
-          <p>Category: <span className={s.desctiptionItem}>test6</span></p>
+          <p>Description: <span className={s.desctiptionItem}>{product[0].description}</span></p>
+          <p>Discount Percentage: <span className={s.desctiptionItem}>{product[0].discountPercentage}</span></p>
+          <p>Rating: <span className={s.desctiptionItem}>{product[0].rating}</span></p>
+          <p>Stock: <span className={s.desctiptionItem}>{product[0].stock}</span></p>
+          <p>Brand: <span className={s.desctiptionItem}>{product[0].brand}</span></p>
+          <p>Category: <span className={s.desctiptionItem}>{product[0].category}</span></p>
         </div>
         <div className={s.buttons}>
           <h3>$549</h3>
           <button>ADD TO CARD</button>
           <button>BUY NOW</button>
+        </div>
       </div>
-      </div>
-      
+
     </div>
   )
 }
