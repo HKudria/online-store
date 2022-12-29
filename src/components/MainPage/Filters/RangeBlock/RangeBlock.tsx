2af8b80056db3ae67feaf35fd0b02d51fab169ba
Herman Kudria <h.kudrya@hotmail.com>
2022-12-29 17:23:27 +0100

    fix: change isNaN to isFinite

--- src/components/MainPage/Filters/RangeBlock/RangeBlock.tsx
+++ src/components/MainPage/Filters/RangeBlock/RangeBlock.tsx
@@ -15,10 +15,11 @@ export const RangeBlock = ({ title, from, to, onChange, initMax }: IRangeProps) 
     <div className={s.rangeWrapper}>
       <h2 className={s.title}>{title}</h2>
       <div className={s.diapason}>
-        <p>0 | {isNaN(from) ? from : 0 }</p>
+
+        <p>0 | {isFinite(from) ? from : 0 }</p>
         <p>⟷</p>
         <p>
-          {isNaN(to) ? to : initMax} | {initMax}
+          {isFinite(to) ? to : initMax} | {initMax}
         </p>
       </div>
       <div className={s.rangeSliderWrapper}>{RangeSlider(from, to, onChange, initMax)}</div>
